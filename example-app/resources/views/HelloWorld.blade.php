91e491141ff9586f6786be2fe2aecbcc5d379611
Carlos Valderrama <81756261+ascxiao@users.noreply.github.com>
2025-01-28 18:51:43 +0800

    modified for the Hello World assignment

---
+++ example-app/resources/views/HelloWorld.blade.php
@@ -0,0 +1,5 @@
+<h1> HELLO WORLD!! </h1>
+
+<h2> hello also sir mapa </h2>
+
+<b> ngayo kami sponsor sa CS Night hehe </b>
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return view('hello');
+Route::get('/HelloWorld', function () {
+    return view('HelloWorld');
 });
