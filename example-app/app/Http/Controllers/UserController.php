22a65cff62167f1ad53c20afbde30db455fa59a6
Carlos Valderrama <81756261+ascxiao@users.noreply.github.com>
2025-02-12 14:32:22 +0800

    added stuff

--- example-app/app/Http/Controllers/UserController.php
+++ example-app/app/Http/Controllers/UserController.php
@@ -9,9 +9,19 @@ class UserController extends Controller
     //Handling POST Request
 
     public function submitForm(Request $request) {
+        $request-> validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',        
+        ]);
+
+        $user-> User::create([
+            'name' => $request->name,
+            'email' => $request->email,        
+        ]);
+
         return response() -> json([
             'message' => 'Form submitted successfully',
-            'data' => $request->all()
-        ]);
+            'user'=>$user
+        ],201);
     }
 }
